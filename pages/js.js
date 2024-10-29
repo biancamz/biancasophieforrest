14936d41418054d404471fe1c80918e5f71e9327
biancamz <bime1968@colorado.edu>
2024-10-29 15:16:52 -0600

    java

--- pages/js.js
+++ pages/js.js
@@ -20,5 +20,5 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";  
     dots[slideIndex - 1].className += " active";
 
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
+    setTimeout(showSlides, 3000); 
 }
