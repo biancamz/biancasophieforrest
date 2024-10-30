277bed12089a889d0e9bdef16f6a36838b647ec7
biancamz <bime1968@colorado.edu>
2024-10-29 18:32:08 -0600

    Update js.js

--- pages/js.js
+++ pages/js.js
@@ -20,5 +20,5 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";  
     dots[slideIndex - 1].className += " active";
 
-    setTimeout(showSlides, 3000); 
+    setTimeout(showSlides, 2000); 
 }
