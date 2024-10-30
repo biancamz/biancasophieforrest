7f593f2e2198a583662f9c9ed2f0b731a5cd8eab
biancamz <bime1968@colorado.edu>
2024-10-30 11:51:12 -0600

    updates

--- pages/js.js
+++ pages/js.js
@@ -4,7 +4,6 @@ showSlides();
 function showSlides() {
     let i;
     let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
 
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
@@ -13,12 +12,12 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
 
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
 
     slides[slideIndex - 1].style.display = "block";  
     dots[slideIndex - 1].className += " active";
 
-    setTimeout(showSlides, 2000); 
+    setTimeout(showSlides, 4000); 
+
 }
+
+
