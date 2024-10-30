f5d827e2ed76b3e1a61d2f37a9ede867b0649f1d
biancamz <bime1968@colorado.edu>
2024-10-30 16:36:40 -0600

    slides

--- pages/js.js
+++ pages/js.js
@@ -1,37 +1,21 @@
 let slideIndex = 0;
-showSlides();
 
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
+    function showSlides() {
+        const slides = document.querySelectorAll(".mySlides");
+        
+        slides.forEach(slide => {
+            slide.classList.remove("active");
+        });
 
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
+        slideIndex++;
+        if (slideIndex > slides.length - 1) {
+            slideIndex = 0; 
+        }
+
+        slides[slideIndex].classList.add("active");
+
+        setTimeout(showSlides, 3000);
     }
 
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-
-
-    slides[slideIndex - 1].style.display = "block";  
-    dots[slideIndex - 1].className += " active";
-
-    setTimeout(showSlides, 4000); 
-
-}
-
-function showImage(elemId, imgSrc) {
-    const elem = document.getElementById(elemId);
-    const popImage = new Image();
-    popImage.src = imgSrc; // Using the provided GitHub Pages URL
-    popImage.className = "hover-image";
-    popImage.style.top = elem.getBoundingClientRect().bottom + window.scrollY + "px"; // Position below the element
-    popImage.style.left = elem.getBoundingClientRect().left + window.scrollX + "px"; // Align left with the element
-    document.body.appendChild(popImage);
-}
-
-function hideImage(elemId) {
-    const popImages = document.querySelectorAll('.hover-image');
-    popImages.forEach(img => img.remove());
-}
+    showSlides();
   
