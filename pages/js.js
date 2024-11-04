30950a73e71cdde84410fa2ae3b74c770b0cc4c7
socr6902 <socr6902@colorado.edu>
2024-11-03 21:11:33 -0700

    linking script to js.js

--- pages/js.js
+++ pages/js.js
@@ -19,3 +19,17 @@ let slideIndex = 0;
 
     showSlides();
   
+    document.addEventListener("DOMContentLoaded", () => {
+        document.querySelectorAll('.accordion-header').forEach(button => {
+            button.addEventListener('click', () => {
+                const content = button.nextElementSibling;
+                button.classList.toggle('active');
+
+                if (content.style.maxHeight) {
+                    content.style.maxHeight = null;
+                } else {
+                    content.style.maxHeight = content.scrollHeight + 'px';
+                }
+            });
+        });
+    });
